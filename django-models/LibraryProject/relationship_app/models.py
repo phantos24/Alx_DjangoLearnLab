5f785914f6891bc1798a865b3e11dce5edb45280
Phantos <mahmoudgamalsad@gmail.com>
2024-11-10 01:41:50 +0200

    adding django-models

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -4,13 +4,13 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
-class Books(models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author,on_delete=models.CASCADE,related_name='books')
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Books,related_name='library')
+    books = models.ManyToManyField(Book,related_name='library')
 
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
