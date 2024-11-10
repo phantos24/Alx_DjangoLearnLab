521c1ea7250efc4e1070a5d597d278537d1754a5
Phantos <mahmoudgamalsad@gmail.com>
2024-11-10 02:14:37 +0200

    adding django-models

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from models import Book
 # Create your views here.
+
+def book_list(request):
+    books = Book.objects.all()
+    list = {'book_list' : books}
+    return render(request, 'books/book_list.html',list)
