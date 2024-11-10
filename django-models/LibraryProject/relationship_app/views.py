f59b40b2ef4700e30206ceb6cd8340bcf2517b73
Phantos <mahmoudgamalsad@gmail.com>
2024-11-10 02:27:14 +0200

    adding django-models

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from models import Book
-from models import Library
+from .models import Library
 from django.views.generic import DetailView
 # Create your views here.
 
