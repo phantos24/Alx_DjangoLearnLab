101d57a7ffa0aca3e4051db238e6645081e8a5a4
Phantos <mahmoudgamalsad@gmail.com>
2024-11-16 19:42:41 +0200

    editing view

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -15,4 +15,7 @@ urlpatterns = [
     path('admin/', views.admin_view, name='admin_view'),
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
+    path('book/add/', views.add_book, name='add_book'),
+    path('book/edit/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('book/delete/<int:book_id>/', views.delete_book, name='delete_book'),
 ]
