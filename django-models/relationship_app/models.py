99a3cb9abc6f12b0ac7cfea4b9b482499e19c652
Phantos <mahmoudgamalsad@gmail.com>
2024-11-16 17:44:25 +0200

    editing view

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Author(models.Model):
@@ -27,3 +28,18 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+    
+class UserProfile (models.Model):
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='UserProfile')
+    role = models.CharField(max_length=100,
+                            choices=ROLE_CHOICES,
+                            default='Member',)
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+    
