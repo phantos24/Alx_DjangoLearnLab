528edf243bf113ae9f38777e0a63aa2a64a6a416
Phantos <mahmoudgamalsad@gmail.com>
2024-11-16 17:04:23 +0200

    editing view

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+from django.urls import path
 # Create your views here.
 
 def list_books(request):
@@ -13,3 +20,16 @@ def list_books(request):
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'relationship_app/signup.html'
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+]
+
+urlpatterns = [
+    path('logout/', LogoutView.as_view(), name='logout'),
+]
