a6c389acf2eae3c798f1c90a7671bbbd03df4031
Phantos <mahmoudgamalsad@gmail.com>
2024-11-16 18:47:00 +0200

    editing view

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -33,4 +33,21 @@ def is_admin(user):
 
 @user_passes_test(is_admin)
 def admin_view(request):
-    return HttpResponse("Welcome to Admin Dashboard")
+    return render(request, 'relationship_app/admin_view.html',
+    {'message' : "Welcome to Admin Dashboard"})
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+@user_passes_test(is_librarian)
+def admin_view(request):
+    return render(request, 'relationship_app/librarian_view.html',
+    {'message' : "Welcome to librarian View"})
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+@user_passes_test(is_member)
+def admin_view(request):
+    return render(request, 'relationship_app/member_view.html',
+    {'message' : "Welcome to member View"})
