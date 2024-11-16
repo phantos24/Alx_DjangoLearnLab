5a8aaee4a303466a4e44b775c364334a42c68525
Phantos <mahmoudgamalsad@gmail.com>
2024-11-16 18:12:12 +0200

    editing view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,6 +7,9 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth import login
 from django.urls import path
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
+from django.http import HttpResponseForbidden
 # Create your views here.
 
 def list_books(request):
@@ -23,3 +26,11 @@ class SignUpView(CreateView):
     form_class = UserCreationForm()
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
+
+
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+@user_passes_test(is_admin, login_url='/forbidden/')
+def admin_view(request):
+    return render(request, 'admin_view.html', {'message': 'Welcome, Admin!'})
