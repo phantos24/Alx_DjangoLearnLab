2bf6c0f43a469985e3c1044c97fa7ae2f7a340b5
Phantos <mahmoudgamalsad@gmail.com>
2024-11-24 01:27:48 +0200

    adding api project

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -129,5 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',  # Example authentication class
+        'rest_framework.permissions.IsAuthenticated'
     ],
 }
