93d27bcbe6b22c2097f83cf711642cbcba7393fe
Phantos <mahmoudgamalsad@gmail.com>
2024-11-24 00:39:12 +0200

    adding api project

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from api.views import BookList
+from api.views import BookViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
+   path('', include(router.urls)),
 ]
