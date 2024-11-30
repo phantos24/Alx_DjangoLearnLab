6e8295b68897bc667811d4976a342625689e467f
Phantos <mahmoudgamalsad@gmail.com>
2024-11-30 14:25:30 +0200

    editing project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,10 @@
+from django.forms import BaseModelForm
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Book
 from django.urls import reverse_lazy
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
 # Create your views here.
 class ListView(ListView):
@@ -14,17 +17,35 @@ class DetailView(DetailView):
     template_name = 'book_detail.html'
     context_object_name = 'book'
 
-class CreateView(CreateView):
+class CreateView(PermissionRequiredMixin, CreateView):
     model = Book
     fields = ['title', 'author', 'publication_year']  
     template_name = 'book_form.html'  
-    success_url = reverse_lazy('book-list')  
+    success_url = reverse_lazy('book-list')
+    permission_required = 'api.add_book'
 
-class UpdateView(UpdateView):
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        return response
+    
+    def form_invalid(self, form):
+        return JsonResponse({'errors': form.errors}, status=400)
+
+
+class UpdateView(PermissionRequiredMixin, UpdateView):
     model = Book
     fields = ['title', 'author', 'publication_year']  
     template_name = 'book_form.html'  
-    success_url = reverse_lazy('book-list') 
+    success_url = reverse_lazy('book-list')
+    permission_required = 'api.change_book'
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        return response
+    
+    def form_invalid(self, form):
+        return JsonResponse({'errors': form.errors}, status=400)
+
 
 class DeleteView(DeleteView):
     model = Book
