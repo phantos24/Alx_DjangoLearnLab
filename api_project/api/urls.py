155ae34ed3bc132b4253a3e70cf90f1252cdae2d
phantos <mahmoudgamalsad@gmail.com>
2024-12-07 17:23:54 +0200

    nothing changed

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from api.views import BookList,BookViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
@@ -9,4 +10,5 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
     path('', include(router.urls)),
+    path('api/token/', obtain_auth_token, name='obtain_token'),
 ]
