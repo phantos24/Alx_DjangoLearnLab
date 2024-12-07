cbd091a218a0022862b4b7513a792ddbb9680e6a
phantos <mahmoudgamalsad@gmail.com>
2024-12-07 19:50:27 +0200

    fixed all errors

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'postgres',
+        'PASSWORD': 'phantos',
+        'HOST': 'localhost',  # or '127.0.0.1' if 'localhost' doesn't work
+        'PORT': '5432',
     }
 }
 
