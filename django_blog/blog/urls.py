92d59d083459c6333178cbdf0a28da3c723aaf98
phantos <mahmoudgamalsad@gmail.com>
2024-12-07 22:40:30 +0200

    addonghandling login logout and profile

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('logout', views.logout, name="logout"),
     path('register', views.register, name="register"),
     path('profile', views.profile, name="profile"),
+    
 ]
