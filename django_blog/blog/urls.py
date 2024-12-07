b6cc7a26b3bf0ec21f17798a0a4db127e4c8ddbf
phantos <mahmoudgamalsad@gmail.com>
2024-12-07 22:52:57 +0200

    handling login logout and profile

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('login/', views.login, name="login"),
-    path('logout/', views.logout, name="logout"),
-    path('register/', views.register, name="register"),
-    path('profile/', views.profile, name="profile"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
+    path("register/", views.register, name="register"),
+    path("profile/", views.profile, name="profile"),
 
 ]
