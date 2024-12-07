f5b8abc17b18f5c5c38456a5595c0fe11514be2d
phantos <mahmoudgamalsad@gmail.com>
2024-12-07 17:22:17 +0200

    blog test

---
+++ django_blog/blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+     def __str__(self):
+        return self
+
+class Post (models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
