28cc304688a5236dcdcdbe869a037d69d7577f69
phantos <mahmoudgamalsad@gmail.com>
2024-12-21 20:19:33 +0200

    adding following feature

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -33,7 +33,7 @@ class FeedView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        followed_users = request.user.following.all()
-        posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
+        following_users = request.user.following.all()
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
